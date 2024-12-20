d7a09ede1788bb560d7dc95938a3a63d997f3d10
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 10:21:37 +0700

    Membuat MataKuliah view model dengan membuat validateFields dan membuat fungsi save data

---
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -0,0 +1,106 @@
+package com.example.ucp2roomdatabase.ui.viewmodel.matakuliah
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2roomdatabase.data.entity.MataKuliah
+import com.example.ucp2roomdatabase.repository.RepositoryMataKuliah
+import kotlinx.coroutines.launch
+
+class MataKuliahViewModel(private val repositoryMataKuliah: RepositoryMataKuliah) : ViewModel() {
+
+    var uiState by mutableStateOf(MKUIState())
+
+    //Memperbarui state berdasarkan input pengguna
+    fun updateState(mataKuliahEvent: MataKuliahEvent) {
+        uiState = uiState.copy(
+            mataKuliahEvent = mataKuliahEvent,
+        )
+    }
+
+    //validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiState.mataKuliahEvent
+        val errorState = FormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData() {
+        val currentEvent = uiState.mataKuliahEvent
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryMataKuliah.insertMatKul(currentEvent.toMataKuliahEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Berhasil Disimpan",
+                        mataKuliahEvent = MataKuliahEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                }
+                catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Gagal Disimpan"
+                    )
+                }
+            }
+        }
+        else{
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak Valid. Periksa Kembali data yang anda masukkan"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
+data class MKUIState(
+    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+data class FormErrorState(
+    val kode: String? = null,
+    val nama: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosenPengampu: String? = null,
+){
+    fun isValid(): Boolean{
+        return kode == null && nama == null && sks == null &&
+                semester == null && jenis == null && dosenPengampu == null
+    }
+}
+
+fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama = nama,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosenPengampu = dosenPengampu
+)
+
+//data class Variable yang menyimpan data input form
+data class MataKuliahEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenis: String = "",
+    val dosenPengampu: String = ""
+)
