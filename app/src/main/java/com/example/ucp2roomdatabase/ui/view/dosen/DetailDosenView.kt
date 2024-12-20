0cad6c2aadf2f49e223c3f2e1370866b1e84cef9
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 21:12:06 +0700

    Menambahkan detail dosen dan fungsi body detail, dan juga memodifikasi card pada detail dosen

--- app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/DetailDosenView.kt
@@ -1,29 +1,129 @@
 package com.example.ucp2roomdatabase.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2roomdatabase.data.entity.Dosen
+import com.example.ucp2roomdatabase.ui.customewidget.TopAppBar
 import com.example.ucp2roomdatabase.ui.viewmodel.dosen.DetailDosenViewModel
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.DetailUiState
 import com.example.ucp2roomdatabase.ui.viewmodel.dosen.PenyediaDosenViewModel
-import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.DetailMKViewModel
-import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.PenyediaMKViewModel
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.toDosenEntity
 
 @Composable
-fun DetailDosenView (
+fun DetailDosenView(
     modifier: Modifier = Modifier,
-    viewModel: DetailDosenViewModel = viewModel (factory = PenyediaDosenViewModel.Factory),
-    onBack: () -> Unit = { },
-    onEditClick: (String) -> Unit = { },
-    onDeleteClick: () -> Unit = { }
-){
+    viewModel: DetailDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
+    onBack: () -> Unit = {},
+) {
+    Scaffold(
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(top = 18.dp),
+        topBar = {
+            TopAppBar(
+                judul = "Detail Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+    ) { innerPadding ->
+        val detailDosenUiState by viewModel.detailUiState.collectAsState()
 
+        BodyDetailDosen(
+            modifier = Modifier.padding(innerPadding),
+            detailDosenUiState = detailDosenUiState,
+        )
+    }
+}
+
+@Composable
+fun BodyDetailDosen(
+    modifier: Modifier = Modifier,
+    detailDosenUiState: DetailUiState = DetailUiState(),
+) {
+    when {
+        detailDosenUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailDosenUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDosen(
+                    dosen = detailDosenUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+        }
+
+        detailDosenUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun ItemDetailDosen(
+    modifier: Modifier = Modifier,
+    dosen: Dosen
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            ComponentDetailDosen(judul = "Nama", isinya = dosen.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDosen(judul = "NIDN", isinya = dosen.nidn)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDosen(judul = "Jenis Kelamin", isinya = dosen.jenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
 }
 
 @Composable
