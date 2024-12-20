d8615978895d16b521ad535999a1a4c9b6c7ca53
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 10:24:27 +0700

    Membuat class detail dosen view model dan mengoneksikan ke repository dosen

---
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2roomdatabase.ui.viewmodel.dosen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.ucp2roomdatabase.repository.RepositoryDosen
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+) :ViewModel(){
+
+
+}
