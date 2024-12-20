129f85e385815342d7f277ba105d11359ab800cb
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 21:07:49 +0700

    Menambahkan Halaman Home dosen dengan Body Home dosen

--- app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/HomeDosenView.kt
@@ -1,30 +1,155 @@
 package com.example.ucp2roomdatabase.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2roomdatabase.data.entity.Dosen
-import com.example.ucp2roomdatabase.data.entity.MataKuliah
-import com.example.ucp2roomdatabase.ui.view.matakuliah.CardMK
+import com.example.ucp2roomdatabase.ui.customewidget.TopAppBar
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.HomeDosenViewModel
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.HomeUiState
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.PenyediaDosenViewModel
+import kotlinx.coroutines.launch
 
+@Composable
+fun HomeDosenView(
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
+    onAddDosen: () -> Unit = {},
+    onBack: () -> Unit,
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(top = 18.dp),
 
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDosen,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeDosenView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+                println(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeDosenView (
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listDsn.isEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data Dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar mahasiswa
+            ListDosen(
+                listDsn = homeUiState.listDsn,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 
 @Composable
 fun ListDosen (
@@ -70,7 +195,7 @@ fun CardDosen (
                 Icon(imageVector = Icons.Filled.Person, contentDescription = "")
                 Spacer(modifier = Modifier.padding(4.dp))
                 Text(
-                    text = dsn.nidn,
+                    text = dsn.nama,
                     fontWeight = FontWeight.Bold,
                     fontSize = 20.sp
                 )
@@ -82,7 +207,7 @@ fun CardDosen (
                 Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
                 Spacer(modifier = Modifier.padding(4.dp))
                 Text(
-                    text = dsn.nama,
+                    text = dsn.nidn,
                     fontWeight = FontWeight.Bold,
                     fontSize = 16.sp
                 )
@@ -91,7 +216,7 @@ fun CardDosen (
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "")
                 Spacer(modifier = Modifier.padding(4.dp))
                 Text(
                     text = dsn.jenisKelamin,
