98c5d1ed63ec566e4d2bb240aed87b22a29f08e0
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 18:05:44 +0700

    Membuat ComponentDetailDosen

--- app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/view/dosen/DetailDosenView.kt
@@ -1,7 +1,14 @@
 package com.example.ucp2roomdatabase.ui.view.dosen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2roomdatabase.ui.viewmodel.dosen.DetailDosenViewModel
 import com.example.ucp2roomdatabase.ui.viewmodel.dosen.PenyediaDosenViewModel
@@ -17,4 +24,28 @@ fun DetailDosenView (
     onDeleteClick: () -> Unit = { }
 ){
 
+}
+
+@Composable
+fun ComponentDetailDosen (
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
