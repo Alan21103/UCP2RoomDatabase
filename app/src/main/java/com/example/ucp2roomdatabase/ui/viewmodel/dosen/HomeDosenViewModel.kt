0c166ae89e7f197ef6763314c47aa411cd0e3212
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 14:43:44 +0700

    Implementasi Pengelolaan UI State untuk Halaman Home Dosen

--- app/src/main/java/com/example/ucp2roomdatabase/ui/viewmodel/dosen/HomeDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -1,8 +1,52 @@
 package com.example.ucp2roomdatabase.ui.viewmodel.dosen
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2roomdatabase.data.entity.Dosen
+import com.example.ucp2roomdatabase.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 
+class HomeDosenViewModel(
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryDosen.getAllDosen()
+        .filterNotNull() // Menghapus data null dari flow
+        .map {
+            HomeUiState(
+                listDsn = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            // Emit state loading sebelum data mulai diterima
+            emit(HomeUiState(isLoading = true))
+            delay(900) // Simulasi delay
+        }
+        .catch {
+            // Menangkap error dan mengupdate UI state dengan pesan error
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(isLoading = true)
+        )
+}
 data class HomeUiState(
     val listDsn: List<Dosen> = listOf(),
     val isLoading: Boolean = false,
