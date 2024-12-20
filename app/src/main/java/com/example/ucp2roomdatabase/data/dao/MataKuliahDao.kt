4428053ede1d51d43cdee05cdfe8ec0edf976362
Alan21103 <syahlansaputra03@gmail.com>
2024-12-20 10:19:54 +0700

    Melakukan penambahan import dao pada matakuliah dao

--- app/src/main/java/com/example/ucp2roomdatabase/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/data/dao/MataKuliahDao.kt
@@ -1,5 +1,6 @@
 package com.example.ucp2roomdatabase.data.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
@@ -7,6 +8,7 @@ import androidx.room.Update
 import com.example.ucp2roomdatabase.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface MataKuliahDao {
     @Insert
     suspend fun insertMatkul(
