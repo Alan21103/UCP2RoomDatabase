824f92a4e263c1cc1ff7e0c4c2c3bea21f98c111
Alan21103 <syahlansaputra03@gmail.com>
2024-12-21 13:23:13 +0700

    Menambahkan fungsi dosen list yang mengambil data pada tabel dosen untuk menginput kan data pada dropdown dosen pengampu

--- app/src/main/java/com/example/ucp2roomdatabase/ui/view/matakuliah/InsertMKView.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/view/matakuliah/InsertMKView.kt
@@ -2,6 +2,7 @@ package com.example.ucp2roomdatabase.ui.view.matakuliah
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,12 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,11 +28,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.ucp2roomdatabase.data.`object`.Sks
-import com.example.ucp2roomdatabase.data.`object`.Semester
+import com.example.ucp2roomdatabase.data.entity.Dosen
 import com.example.ucp2roomdatabase.ui.customewidget.TopAppBar
 import com.example.ucp2roomdatabase.ui.navigation.AlamatNavigasi
 import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.FormErrorState
@@ -37,8 +38,9 @@ import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.MKUIState
 import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.MataKuliahEvent
 import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.MataKuliahViewModel
 import com.example.ucp2roomdatabase.ui.customewidget.DynamicSelectedTextField
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.HomeDosenViewModel
+import com.example.ucp2roomdatabase.ui.viewmodel.dosen.HomeUiState
 import com.example.ucp2roomdatabase.ui.viewmodel.dosen.PenyediaDosenViewModel
-import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.HomeMKViewModel
 import com.example.ucp2roomdatabase.ui.viewmodel.matakuliah.PenyediaMKViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -54,11 +56,12 @@ fun InsertMKView(
     onNavigate: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: MataKuliahViewModel = viewModel(factory = PenyediaMKViewModel.Factory),
-    viewModelDsn: HomeMKViewModel =viewModel(factory = PenyediaDosenViewModel.Factory)
+    viewModelDsn: HomeDosenViewModel =viewModel(factory = PenyediaDosenViewModel.Factory)
 ) {
     val uiState = viewModel.uiState
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+    val dsnList by viewModelDsn.homeUiState.collectAsState()
 
     LaunchedEffect(uiState.snackBarMessage) {
         uiState.snackBarMessage?.let { message ->
@@ -89,6 +92,7 @@ fun InsertMKView(
         ) {
             InsertBodyMK(
                 uiState = uiState,
+                listDosen = dsnList,
                 onValueChange = { updateEvent ->
                     viewModel.updateState(updateEvent)
                 },
@@ -113,7 +117,8 @@ fun InsertBodyMK(
     modifier: Modifier = Modifier,
     onValueChange: (MataKuliahEvent) -> Unit,
     uiState: MKUIState,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    listDosen: HomeUiState
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
@@ -124,7 +129,8 @@ fun InsertBodyMK(
             mataKuliahEvent = uiState.mataKuliahEvent,
             onValueChange = onValueChange,
             errorState = uiState.isEntryValid,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            listDsn = listDosen.listDsn
         )
         Button(
             onClick = onClick,
@@ -136,19 +142,21 @@ fun InsertBodyMK(
 }
 
 
-@Preview(showBackground = true)
 @Composable
 fun FormMataKuliah(
     mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
     onValueChange: (MataKuliahEvent) -> Unit = {},
     errorState: FormErrorState = FormErrorState(),
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    listDsn: List<Dosen>
 ){
     var chosenDropdown by remember {
         mutableStateOf(
             ""
         )
     }
+    val jenis = listOf("Wajib", "Peminatan")
+    val namaDosenList = listDsn.map { it.nama }
 
     Column(
         modifier = modifier.fillMaxWidth()
@@ -186,54 +194,80 @@ fun FormMataKuliah(
         )
 
         Spacer(modifier = Modifier.height(10.dp))
-        DynamicSelectedTextField(
-            selectedValue = chosenDropdown,
-            options = Sks.options,
-            label = "SKS",
-            onValueChangedEvent = {
-                chosenDropdown = it
-            }
-        )
 
-        Spacer(modifier = Modifier.height(10.dp))
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
-            value = mataKuliahEvent.jenis,
+            value = mataKuliahEvent.sks,
             onValueChange = {
-                onValueChange(mataKuliahEvent.copy(jenis = it))
+                onValueChange(mataKuliahEvent.copy(sks = it))
             },
-            label = { Text("Jenis") },
-            isError = errorState.jenis !=null,
-            placeholder = { Text("Masukkan Jenis") },
+            label = { Text("SKS") },
+            isError = errorState.sks !=null,
+            placeholder = { Text("Masukkan SKS MataKuliah") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         Text(
-            text = errorState.jenis ?:"",
+            text = errorState.sks ?:"",
             color = Color.Red
         )
 
         Spacer(modifier = Modifier.height(10.dp))
-        DynamicSelectedTextField(
-            selectedValue = chosenDropdown,
-            options = Semester.options,
-            label = "Semester",
-            onValueChangedEvent = {
-                chosenDropdown = it
+
+        Text(text = "Jenis")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            jenis.forEach { jenisOption ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = mataKuliahEvent.jenis == jenisOption,
+                        onClick = {
+                            onValueChange(mataKuliahEvent.copy(jenis = jenisOption))
+                        },
+                    )
+                    Text(text = jenisOption)
+                }
             }
+        }
+        Text(
+            text = errorState.jenis ?: "",
+            color = Color.Red
         )
+
         Spacer(modifier = Modifier.height(10.dp))
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
-            value = mataKuliahEvent.dosenPengampu,
+            value = mataKuliahEvent.semester,
             onValueChange = {
-                onValueChange(mataKuliahEvent.copy(dosenPengampu = it))
+                onValueChange(mataKuliahEvent.copy(semester = it))
             },
-            label = { Text("Dosen Pengampu") },
-            isError = errorState.dosenPengampu !=null,
-            placeholder = { Text("Pilih Dosen Pengampu") },
+            label = { Text("Semester") },
+            isError = errorState.semester !=null,
+            placeholder = { Text("Masukkan Semester MataKuliah") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
         )
         Text(
-            text = errorState.dosenPengampu ?:"",
+            text = errorState.semester ?:"",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "Dosen Pengampu")
+        DynamicSelectedTextField(
+            selectedValue = mataKuliahEvent.dosenPengampu,
+            options = namaDosenList,
+            label = "Pilih Dosen Pengampu",
+            onValueChangedEvent = {
+                onValueChange(mataKuliahEvent.copy(dosenPengampu = it))
+            }
+        )
+        Text(
+            text = errorState.dosenPengampu ?: "",
             color = Color.Red
         )
     }
