af54756c880082291477d7dc392b1e39ff947718
Alan21103 <syahlansaputra03@gmail.com>
2024-12-22 17:03:48 +0700

    Memodifikasi top app bar dengan menambah icon

--- app/src/main/java/com/example/ucp2roomdatabase/ui/customewidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2roomdatabase/ui/customewidget/TopAppBar.kt
@@ -1,19 +1,26 @@
 package com.example.ucp2roomdatabase.ui.customewidget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2roomdatabase.R
 
 @Composable
 fun TopAppBar(
@@ -25,28 +32,36 @@ fun TopAppBar(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .background(color = colorResource(id = R.color.purple_500)
+            )
             .padding(16.dp),
-        contentAlignment = Alignment.Center // Pastikan konten di tengah
+        contentAlignment = Alignment.Center
     ) {
         if (showBackButton) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterStart),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                TextButton(
+                IconButton(
                     onClick = onBack,
-                    modifier = Modifier.align(Alignment.CenterVertically)
+                    modifier = Modifier.size(48.dp) // Ukuran tombol ikon
                 ) {
-                    Text("Kembali")
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack, // Ikon kembali
+                        contentDescription = "Kembali",
+                        tint = colorResource(id = R.color.white) // Warna ikon
+                    )
                 }
-                Spacer(modifier = Modifier.weight(2f))
             }
         }
-        // Teks judul
+        // Teks judul di tengah
         Text(
             text = judul,
-            fontSize = 20.sp,
+            fontSize = 25.sp,
             fontWeight = FontWeight.Bold,
+            color = Color.White,
             modifier = Modifier.align(Alignment.Center)
         )
     }
